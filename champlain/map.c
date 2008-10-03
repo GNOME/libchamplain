9caeb28f7cd75f0c368285fa8e298862b182705d
Pierre-Luc Beaudoin <pierre-luc@squidy.info>
2008-10-03 14:29:43 -0400

    Get rid of squidy.info

--- champlain/map.c
+++ champlain/map.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Pierre-Luc Beaudoin <pierre-luc@squidy.info>
+ * Copyright (C) 2008 Pierre-Luc Beaudoin <pierre-luc@pierlux.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
