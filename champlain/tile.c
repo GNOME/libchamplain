70b95834dd02f6eff169d6d474b06cb762ec39a9
Pierre-Luc Beaudoin <pierre-luc@pierlux.com>
2009-02-09 22:20:55 +0200

    Don't generate a million criticals if the image is not found

--- champlain/tile.c
+++ champlain/tile.c
@@ -56,6 +56,8 @@ static void
 create_error_tile(Map* map, Tile* tile)
 {
   tile->actor = clutter_texture_new_from_file(DATADIR "/champlain/error.svg", NULL);
+  if (!tile->actor)
+    return;
 
   tile_set_position(map, tile);
 
