c5a67e529afd60c247f3ccec1e078ff5567e64ea
Michael Sheldon <mike@mikeasoft.com>
2009-02-26 19:59:52 +0200

    Fix up the bindings a little

---
+++ bindings/python/champlain-gtk/pychamplaingtkmodule.c
@@ -0,0 +1,26 @@
+#define NO_IMPORT_PYGOBJECT
+
+#include <pygobject.h>
+
+void champlain_register_classes (PyObject *d);
+DL_EXPORT(void) initchamplaingtk(void);
+extern PyMethodDef champlain_functions[];
+
+DL_EXPORT(void)
+initchamplaingtk(void)
+{
+	PyObject *m, *d;
+
+	init_pygobject ();
+	
+	m = Py_InitModule ("champlaingtk", champlain_functions);
+	d = PyModule_GetDict (m);
+	
+	champlain_register_classes (d);
+	
+	if (PyErr_Occurred ()) {
+		PyErr_Print();
+		Py_FatalError ("can't initialise module champlaingtk");
+	}
+}
+
