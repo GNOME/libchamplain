c8d4b754fb316cf620dcd8a933e3e9886103d878
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-03-16 19:27:10 +0100

    Move the include file around.

--- bindings/perl/Champlain/champlain-perl.h
+++ bindings/perl/Champlain/champlain-perl.h
@@ -1,12 +1,16 @@
 #ifndef _CHAMPLAIN_PERL_H_
 
 #include <clutterperl.h>
+
+
 #include <champlain/champlain.h>
-#include "ppport.h"
-#include "champlain-autogen.h"
 
 #ifdef CHAMPLAINPERL_GTK
 #include <champlain-gtk/champlain-gtk.h>
 #endif
+
+
+#include "ppport.h"
+#include "champlain-autogen.h"
 
 #endif /* _CHAMPLAIN_PERL_H_ */
