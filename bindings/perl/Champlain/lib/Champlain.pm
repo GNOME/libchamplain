31133064e1b44bff40ed639cf8df672777ca7fb9
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-03-25 22:06:21 +0100

    Version change to 0.4

--- bindings/perl/Champlain/lib/Champlain.pm
+++ bindings/perl/Champlain/lib/Champlain.pm
@@ -92,7 +92,7 @@ at your option, any later version of Perl 5 you may have available.
 use warnings;
 use strict;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 use base 'DynaLoader';
 use Exporter 'import';
