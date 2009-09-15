86d74620e2bb7877b1963be95977a9b4f63f3b3e
Emmanuel Rodriguez <emmanuel.rodriguez@booking.com>
2009-09-15 20:26:25 +0200

    The Perl bindings for champlain-gtk will be in a different package

--- bindings/perl/Champlain/champlain-perl.h
+++ bindings/perl/Champlain/champlain-perl.h
@@ -2,14 +2,7 @@
 
 #include <clutter-perl.h>
 
-
 #include <champlain/champlain.h>
-
-#ifdef CHAMPLAINPERL_GTK
-#include <gtk2perl.h>
-#include <champlain-gtk/champlain-gtk.h>
-#endif
-
 
 #include "champlain-autogen.h"
 
