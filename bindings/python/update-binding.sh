18dfa68d0263d53ec75da583f20cbe89207def9e
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2009-11-22 18:24:21 -0200

    python binding: removes GInitiallyUnowned bad hack and cleans update-binding.sh script

--- bindings/python/update-binding.sh
+++ bindings/python/update-binding.sh
@@ -20,7 +20,7 @@ python /usr/share/pygobject/2.0/codegen/h2def.py	\
 	champlain-base-marker.h			\
 	champlain-tile.h			\
 	champlain-zoom-level.h			\
-| sed "s/GInitiallyUnowned/GObject/g" > ../bindings/python/champlain/pychamplain-base.defs
+ > ../bindings/python/champlain/pychamplain-base.defs
 
 
 # Update the list of headers from Makefile.am
@@ -33,14 +33,5 @@ python /usr/share/pygobject/2.0/codegen/h2def.py	\
 
 # Keep original version
 cd ../bindings/python
-cp champlain/pychamplain.defs /tmp
+cp champlain/pychamplain-base.defs /tmp
 cp champlain-gtk/pychamplaingtk.defs /tmp
-
-# Apply patches
-#patch -p0 < pychamplain.patch
-#patch -p0 < pychamplaingtk.patch
-
-# Make modification then run that:
-#diff -up /tmp/pychamplain.defs champlain/pychamplain.defs > pychamplain.patch
-#diff -up /tmp/pychamplaingtk.defs champlain-gtk/pychamplaingtk.defs > pychamplaingtk.patch
-
