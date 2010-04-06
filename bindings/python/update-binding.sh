f854efe88ecaaab3371407c8bb51f193189cf68d
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2010-04-05 23:51:19 -0300

    Add support to virtual methods by using defsgen.py.
    Reported by Jiří Techet.

--- bindings/python/update-binding.sh
+++ bindings/python/update-binding.sh
@@ -1,38 +1,45 @@
 #! /bin/sh
-#Manually update headers in pychamplain.override and pychamplaingtk.override.
+# Manually update headers in pychamplain.override and pychamplaingtk.override.
+# Keep original version
+cp champlain/pychamplain-base.defs /tmp/pychamplain-base.defs.bk
+cp champlain-gtk/pychamplaingtk-base.defs /tmp/pychamplaingtk-base.defs.bk
 
 # Update the list of headers from Makefile.am
 cd ../../champlain
-python /usr/share/pygobject/2.0/codegen/h2def.py	\
-	-m champlain				\
-	champlain.h				\
-	champlain-cache.h			\
-	champlain-view.h			\
-	champlain-defines.h			\
-	champlain-polygon.h			\
-	champlain-point.h			\
-	champlain-layer.h			\
-	champlain-selection-layer.h		\
-	champlain-map-source.h			\
-	champlain-map-source-desc.h		\
-	champlain-map-source-factory.h		\
-	champlain-network-map-source.h		\
-	champlain-marker.h			\
-	champlain-base-marker.h			\
-	champlain-tile.h			\
-	champlain-zoom-level.h			\
- > ../bindings/python/champlain/pychamplain-base.defs
-
+python /usr/share/pygobject/2.0/codegen/defsgen.py \
+    -m champlain -l .libs/libchamplain-0.5.so \
+    -f ../bindings/python/champlain/pychamplain-boxed-types.defs \
+	champlain.h 			\
+	champlain-defines.h		\
+	champlain-point.h		\
+	champlain-view.h		\
+	champlain-layer.h 		\
+	champlain-selection-layer.h 	\
+	champlain-base-marker.h		\
+	champlain-marker.h		\
+	champlain-tile.h		\
+	champlain-map-source.h		\
+	champlain-map-source-chain.h	\
+	champlain-tile-source.h		\
+	champlain-tile-cache.h		\
+	champlain-network-tile-source.h	\
+	champlain-error-tile-source.h	\
+	champlain-file-cache.h		\
+	champlain-map-source-factory.h	\
+	champlain-map-source-desc.h	\
+	champlain-polygon.h		\
+	champlain-version.h		\
+	champlain-memphis-tile-source.h  \
+	champlain-map-data-source.h	\
+	champlain-local-map-data-source.h \
+	champlain-network-map-data-source.h \
+	champlain-bounding-box.h \
+    > ../bindings/python/champlain/pychamplain-base.defs
 
 # Update the list of headers from Makefile.am
 cd ../champlain-gtk
-python /usr/share/pygobject/2.0/codegen/h2def.py	\
-	-m champlain				\
+python /usr/share/pygobject/2.0/codegen/defsgen.py	\
+    -m champlain -l .libs/libchamplain-gtk-0.5.so \
 	champlain-gtk.h				\
 	gtk-champlain-embed.h			\
- > ../bindings/python/champlain-gtk/pychamplaingtk.defs
-
-# Keep original version
-cd ../bindings/python
-cp champlain/pychamplain-base.defs /tmp
-cp champlain-gtk/pychamplaingtk.defs /tmp
+    > ../bindings/python/champlain-gtk/pychamplaingtk-base.defs
