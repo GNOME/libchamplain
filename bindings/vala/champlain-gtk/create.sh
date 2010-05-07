e756e111a9f92d8540e07d7b420cbc922323a075
Simon Wenner <simon@wenner.ch>
2010-05-08 01:32:23 +0200

    Vala-bindings: Add several missing files
    - The *.deps files describe the dependencies of a vapi file and should be installed too.
    - The *.defines files are needed to regenerate the *.gi files without errors.
    - create.sh should parse the metadata file.

--- bindings/vala/champlain-gtk/create.sh
+++ bindings/vala/champlain-gtk/create.sh
@@ -1,1 +1,1 @@
-vapigen --library champlain-gtk-0.4 --pkg clutter-gtk-0.10 --pkg gtk+-2.0 --pkg champlain-0.4 --vapidir=../champlain-0.4 champlain-gtk-0.4/champlain-gtk-0.4.gi
+vapigen --library champlain-gtk-0.4 --pkg clutter-gtk-0.10 --pkg gtk+-2.0 --pkg champlain-0.4 --vapidir=../champlain-0.4 champlain-gtk-0.4/champlain-gtk-0.4.gi --metadata champlain-gtk-0.4/champlain-gtk-0.4.metadata
