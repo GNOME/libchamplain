4e963c9af3a59b9da886742c6b485b84960e42b9
Jiří Techet <techet@gmail.com>
2010-05-22 00:38:49 +0200

    Add minimal demos
    Signed-off-by: Jiří Techet <techet@gmail.com>

---
+++ demos/minimal-gtk.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2010 Jiri Techet <techet@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <gtk/gtk.h>
+
+/* include the libchamplain header */
+#include <champlain-gtk/champlain-gtk.h>
+
+#include <clutter-gtk/clutter-gtk.h>
+
+int
+main (int argc, char *argv[])
+{
+  GtkWidget *window, *widget, *vbox;
+
+  /* initialize threads and clutter */
+  g_thread_init (NULL);
+  gtk_clutter_init (&argc, &argv);
+
+  /* create the top-level window and quit the main loop when it's closed */
+  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  g_signal_connect (G_OBJECT (window), "destroy", G_CALLBACK (gtk_main_quit),
+      NULL);
+
+  /* create the libchamplain widget and set its size */
+  widget = gtk_champlain_embed_new ();
+  gtk_widget_set_size_request (widget, 640, 480);
+
+  ChamplainView *view = gtk_champlain_embed_get_view (GTK_CHAMPLAIN_EMBED (widget));
+  g_object_set (G_OBJECT (view), "scroll-mode", CHAMPLAIN_SCROLL_MODE_KINETIC,
+          "zoom-level", 5, NULL);
+  champlain_view_center_on(CHAMPLAIN_VIEW(view), 45.466, -73.75);
+
+  /* insert it into the widget you wish */
+  vbox = gtk_vbox_new(FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (vbox), widget, TRUE, TRUE, 0);
+  gtk_container_add (GTK_CONTAINER (window), vbox);
+
+  /* show everything */
+  gtk_widget_show_all (window);
+
+  /* start the main loop */
+  gtk_main ();
+
+  return 0;
+}
---
+++ demos/minimal.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2010 Jiri Techet <techet@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <champlain/champlain.h>
+
+int
+main (int argc, char *argv[])
+{
+  ClutterActor* actor, *stage;
+
+  g_thread_init (NULL);
+  clutter_init (&argc, &argv);
+
+  stage = clutter_stage_get_default ();
+  clutter_actor_set_size (stage, 800, 600);
+
+  /* Create the map view */
+  actor = champlain_view_new ();
+  clutter_actor_set_size (CLUTTER_ACTOR (actor), 800, 600);
+  clutter_container_add_actor (CLUTTER_CONTAINER (stage), actor);
+
+  clutter_actor_show (stage);
+  clutter_main ();
+
+  clutter_actor_destroy (actor);
+    
+  return 0;
+}
