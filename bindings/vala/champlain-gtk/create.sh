3df63cb43d6bc7257ee1bc0fe0d2cc4ee461b929
Jiří Techet <techet@gmail.com>
2010-06-02 00:56:47 +0200

    Use automatic version generation for Vala bindings where possible
    + some minor cleanups
    
    Signed-off-by: Jiří Techet <techet@gmail.com>

--- bindings/vala/champlain-gtk/create.sh
+++
@@ -1,4 +0,0 @@
-export PKG_CONFIG_PATH=$(readlink -f ../../../)
-echo $PKG_CONFIG_PATH
-vala-gen-introspect champlain-gtk-0.6 champlain-gtk-0.6
-vapigen --library champlain-gtk-0.6 --pkg clutter-gtk-0.10 --pkg gtk+-2.0 --pkg champlain-0.6 --vapidir=../champlain champlain-gtk-0.6/champlain-gtk-0.6.gi --metadata champlain-gtk-0.6/champlain-gtk-0.6.metadata
