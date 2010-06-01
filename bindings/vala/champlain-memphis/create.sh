68476b593f71d243eb062a7b0c8f53d4d03917df
Jiří Techet <techet@gmail.com>
2010-06-02 00:56:47 +0200

    Use automatic version generation for Vala bindings where possible
    + some minor cleanups
    
    Signed-off-by: Jiří Techet <techet@gmail.com>

--- bindings/vala/champlain-memphis/create.sh
+++
@@ -1,4 +0,0 @@
-export PKG_CONFIG_PATH=$(readlink -f ../../../)
-
-vala-gen-introspect champlain-memphis-0.6 champlain-memphis-0.6
-vapigen --library champlain-memphis-0.6 --pkg champlain-0.6 --vapidir=../champlain champlain-memphis-0.6/champlain-memphis-0.6.gi --metadata champlain-memphis-0.6/champlain-memphis-0.6.metadata
--- bindings/vala/champlain/create.sh
+++
@@ -1,4 +0,0 @@
-export PKG_CONFIG_PATH=$(readlink -f ../../../)
-
-vala-gen-introspect champlain-0.6 champlain-0.6
-vapigen --library champlain-0.6 --pkg clutter-1.0 champlain-0.6/champlain-0.6.gi --metadata champlain-0.6/champlain-0.6.metadata
