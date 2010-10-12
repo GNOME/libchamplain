f3c21121800e26fcb6acbd25fa1299e49732b1c8
William Jon McCann <jmccann@redhat.com>
2010-10-12 01:32:37 -0400

    Remove python bindings
    For GNOME 3 bindings should be generated using introspection
    
    https://bugzilla.gnome.org/show_bug.cgi?id=631009

--- bindings/python/champlain/pychamplain.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PYCHAMPLAIN_H__
-#define __PYCHAMPLAIN_H__
-
-void champlain_register_classes (PyObject *d);
-void champlain_add_constants (PyObject *module, const gchar *strip_prefix);
-
-#endif
--- bindings/python/champlain/pychamplainmemphis.h
+++
@@ -1,7 +0,0 @@
-#ifndef __PYCHAMPLAINMEMPHIS_H__
-#define __PYCHAMPLAINMEMPHIS_H__
-
-void champlainmemphis_register_classes (PyObject *d);
-void champlainmemphis_add_constants (PyObject *module, const gchar *strip_prefix);
-
-#endif
