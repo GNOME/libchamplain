bd9a31502429be162ffc8832b9ad771da5869dcb
Jiří Techet <techet@gmail.com>
2011-12-28 23:44:08 +0100

    Minor changes/cleanups in Python demos

--- demos/markers.py
+++ demos/markers.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from gi.repository import Clutter
 from gi.repository import Champlain
@@ -29,24 +28,24 @@ def create_marker_layer(view):
     marker.connect("button-release-event", marker_button_release_cb, view)
 
     marker = Champlain.Label.new_from_file(
-        "/usr/share/icons/gnome/24x24/emblems/emblem-generic.png")
+        "icons/emblem-generic.png")
     marker.set_text("New York")
     marker.set_location(40.77, -73.98)
     layer.add_marker(marker)
 
     marker = Champlain.Label.new_from_file(
-        "/usr/share/icons/gnome/24x24/emblems/emblem-important.png")
+        "icons/emblem-important.png")
     marker.set_location(47.130885, -70.764141)
     layer.add_marker(marker)
 
     marker = Champlain.Label.new_from_file(
-        "/usr/share/icons/gnome/24x24/emblems/emblem-favorite.png")
+        "icons/emblem-favorite.png")
     marker.set_draw_background(False)
     marker.set_location(45.41484, -71.918907)
     layer.add_marker(marker)
 
     marker = Champlain.Label.new_from_file(
-        "/usr/share/icons/gnome/24x24/emblems/emblem-new.png")
+        "icons/emblem-new.png")
     marker.set_draw_background(False)
     marker.set_location(50.639663, 5.570798)
     layer.add_marker(marker)
