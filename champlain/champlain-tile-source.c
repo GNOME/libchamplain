270db8f130c955254bf64f12e680d7d2f4e15c3d
Jiří Techet <techet@gmail.com>
2011-12-29 18:55:05 +0100

    It's the time of the year again...

--- champlain/champlain-tile-source.c
+++ champlain/champlain-tile-source.c
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2008-2009 Pierre-Luc Beaudoin <pierre-luc@pierlux.com>
- * Copyright (C) 2010 Jiri Techet <techet@gmail.com>
+ * Copyright (C) 2010-2012 Jiri Techet <techet@gmail.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
