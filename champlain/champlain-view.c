c7f9ed8d38a199245fc80a22328be78000174119
Marius Stanciu <stanciumarius94@gmail.com>
2016-07-18 16:17:15 +0300

    Destroy zoom_actor even if no tiles finished downloading.
    Clones are inserted in the zoom_actor in the show_zoom_actor
    function. In case no new tiles needed to be downloaded after
    zooming, the zoom_actor is not destroyed, so the clones remained
    covered.

--- champlain/champlain-view.c
+++ champlain/champlain-view.c
@@ -3371,6 +3371,9 @@ zoom_animation_completed (ClutterActor *actor,
   if (priv->hwrap)
     update_clones (view);
 
+  if (priv->tiles_loading == 0)
+    clutter_actor_destroy_all_children (priv->zoom_layer);
+
   g_signal_handlers_disconnect_by_func (actor, zoom_animation_completed, view);
   g_signal_emit_by_name (view, "animation-completed::zoom", NULL);
 }
